9ffd61baf4e8147c1e7cad87793f46b657116c46
Leonard Matheus <leo.matt.547@gmail.com>
2021-03-23 13:00:23 +0700

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -2,20 +2,19 @@
 #include <iostream>
 using namespace std;
 
-Map::Map(){
+Map::Map() : Position(8,8){
     xmax = 12;
     ymax = 12;
     setCapital = 5;
     dimensiWater = 6;
-    player_position = Position(8,8);
 }
 void Map::printMap(){
     for (int i = 0; i < xmax; i++){
         for (int j = 0; j < ymax; j++){
-            if(i > xmax-dimensiWater || j > ymax-dimensiWater){
-                cout << "0 ";
-            }else{
+            if(i > xmax-dimensiWater || j < ymax-dimensiWater){
                 cout << "- ";
+            }else{
+                cout << "0 ";
             }
         }
         cout << endl;
@@ -24,6 +23,7 @@ void Map::printMap(){
 
 int main(int argc, char const *argv[])
 {
-    printMap();
+    Map m = Map();
+    m.printMap();
     return 0;
 }
