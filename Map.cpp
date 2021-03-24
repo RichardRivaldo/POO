a2d09beacd85845b45977534898df33982dcdc98
leomatt547 <leo.matt.547@gmail.com>
2021-03-24 22:20:26 +0700

    add element

--- Map.cpp
+++ Map.cpp
@@ -1,5 +1,7 @@
 #include "Map.hpp"
 #include <iostream>
+#include <vector>
+#include "battle.cpp"
 using namespace std;
 
 Map::Map(){
@@ -20,7 +22,63 @@ void Map::printMap(){
         for(int j = 0; j < ymax; j++){
             if(i > xmax-dimensiWater || j < ymax-dimensiWater){
                 cout << "- ";
-            }//else if (peta[i][j].getEngimon().getElemen= )
+            }else if (peta.at(i).at(j).getisPlayer()){
+                cout << "P ";
+            }else if (peta.at(i).at(j).getisActiveEngimon()){
+                cout << "X ";
+            }else if (getCurrElement(peta.at(i).at(j)).at(0)=="FIRE"){
+                if (isAboveLevel(peta.at(i).at(j))){
+                    cout << "F ";
+                }else{
+                    cout << "f ";
+                }
+            }
+            else if (getCurrElement(peta.at(i).at(j)).at(0)=="WATER"){
+                if (isAboveLevel(peta.at(i).at(j))){
+                    cout << "W ";
+                }else{
+                    cout << "w ";
+                }
+            }else if (getCurrElement(peta.at(i).at(j)).at(0)=="ICE"){
+                if (isAboveLevel(peta.at(i).at(j))){
+                    cout << "I ";
+                }else{
+                    cout << "i ";
+                }
+            }else if (getCurrElement(peta.at(i).at(j)).at(0)=="GROUND"){
+                if (isAboveLevel(peta.at(i).at(j))){
+                    cout << "G ";
+                }else{
+                    cout << "g ";
+                }
+            }else if (getCurrElement(peta.at(i).at(j)).at(0)=="ELECTRIC"){
+                if (isAboveLevel(peta.at(i).at(j))){
+                    cout << "E ";
+                }else{
+                    cout << "e ";
+                }
+            }else if (getCurrElement(peta.at(i).at(j)).at(0)=="FIRE" 
+                && getCurrElement(peta.at(i).at(j)).at(0)=="ELECTRIC"){
+                if (isAboveLevel(peta.at(i).at(j))){
+                    cout << "L ";
+                }else{
+                    cout << "l ";
+                }
+            }else if (getCurrElement(peta.at(i).at(j)).at(0)=="WATER" 
+                && getCurrElement(peta.at(i).at(j)).at(0)=="ICE"){
+                if (isAboveLevel(peta.at(i).at(j))){
+                    cout << "S ";
+                }else{
+                    cout << "s ";
+                }
+            }else if (getCurrElement(peta.at(i).at(j)).at(0)=="WATER" 
+                && getCurrElement(peta.at(i).at(j)).at(0)=="GROUND"){
+                if (isAboveLevel(peta.at(i).at(j))){
+                    cout << "N ";
+                }else{
+                    cout << "n ";
+                }
+            }
             else{
                 cout << "0 ";
             }
@@ -28,6 +86,21 @@ void Map::printMap(){
         cout << endl;
     }
 }
+
+vector<string> Map::getCurrElement(Position k){
+    return k.getEngimon()->getElement();
+}
+
+bool Map::isAboveLevel(Position k){
+    return (k.getEngimon()->getLevel() >= setCapital);
+}
+
+void Map::movePlayerUp(){
+    if((player_position.getYCoordinate())-1>=0){
+        this->player_position = 
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     Map m = Map();
