bbfe9d746204c65bd9b4c26501a19eddf0beea49
DaveyJones491 <david.owen.491@gmail.com>
2021-03-25 12:27:40 +0700

    Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <iostream>
+#include "Player.hpp"
+
+using namespace std;
+
+Player :: Player(int posx, int posy){
+    this.posx = 0;
+    this.posy = 0;
+}
+
+//Move Command
+void Player::moveUp(){
+
+}
+
+void Player::moveDown(){
+    
+}
+
+void Player::moveLeft(){
+    
+}
+
+void Player::moveRight(){
+    
+}
+
+//Engimon Command
+void Player::showOwnedEngimon(){
+    
+}
+
+void Player::showStatsEngimon(){
+    
+}
+
+void Player::showActiveEngimon(){
+    
+}
+
+void Player::swapActiveEngimon(){
+    
+}
+
+//Skill Command
+void Player::showOwnedSkill(){
+    
+}
+
+void Player::useOwnedSkill(){
+    
+}
+
+//Breed
+void Player::doBreed(){
+    
+}
+
+//Battle
+void Player::doBattle(){
+    
+}
--- Player.hpp
+++ Player.hpp
@@ -6,8 +6,44 @@
 
 using namespace std;
 
+/*  To Do : 
+    Gerak (Engimon mengikuti di belakang)
+    Show List Engimon
+    Show Data Engimon
+    Select, Swap Active Engimon
+    Menggunakan Skill
+    Mengecek dan Mengganti Active Engimon
+    Breeding
+    Battle (prioritas WASD)
+*/
+
 class Player {
     private :
-        Position player_position;
-        Inventory player_inventory;
+        int posx;
+        int posy;
+    public :
+    //Ctor
+    Player(int posx, int posy);
+
+    //Move Command
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+
+    //Engimon Command
+    void showOwnedEngimon();
+    void showStatsEngimon(Engimon a);
+    void showActiveEngimon();
+    void swapActiveEngimon(Engimon b);
+
+    //Skill Command
+    void showOwnedSkill();
+    void useOwnedSkill(Skill x, Engimon y);
+
+    //Breed
+    void doBreed(Engimon a, Engimon b);
+
+    //Battle
+    void doBattle();
 };
